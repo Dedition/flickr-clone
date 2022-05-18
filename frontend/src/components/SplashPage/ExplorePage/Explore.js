31b8a59818403fb1f963ed839e800f06063ee261
Leo Lad <87998262+Dedition@users.noreply.github.com>
2022-05-18 02:45:31 -0400

    Added redirect for images

--- frontend/src/components/SplashPage/ExplorePage/Explore.js
+++ frontend/src/components/SplashPage/ExplorePage/Explore.js
@@ -17,16 +17,24 @@ const Explore = () => {
         dispatch(getImages());
     }, [dispatch]);
 
+    const imageRedirect = (image) => {
+        history.push(`/images/${image.id}`, { from: "explore" });
+    }
+
     return (
         <>
             <h1>Welcome to the Gallery</h1>
             {/* <button onClick={() => history.push('/signup')}>Signup</button>
             <button onClick={() => history.push('/login')}>Login</button>
             <button onClick={() => history.push('/images')}>Images</button> */}
-            <div>
-                {images.map(image => (
-                    <img key={image?.id} src={image?.imageUrl} />
-                ))}
+            <div className="explore-container">
+                <div>
+                    {images.map(image => (
+                        <div className='images' key={image?.id}>
+                            <img onClick={() => { imageRedirect(image) }} key={image?.id} src={image?.imageUrl} />
+                        </div>
+                    ))}
+                </div>
             </div>
         </>
     );
