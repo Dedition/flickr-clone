2b11e87a5960b303c8019a4734fee6047bba2a7f
Leo Lad <87998262+Dedition@users.noreply.github.com>
2022-05-21 13:46:28 -0400

    Fixed issue that stopped page from refreshing when deleting an image

--- frontend/src/components/ImageDetailsPage/ImageDetails.js
+++ frontend/src/components/ImageDetailsPage/ImageDetails.js
@@ -1,10 +1,13 @@
+//*                         React
 import { useEffect } from "react";
 import { useDispatch, useSelector } from "react-redux";
 import { useParams, useHistory } from "react-router-dom";
-import { deleteImageAction } from "../../store/images";
 
+//*                         Store
+import { deleteImageAction } from "../../store/images";
 import { getImages } from "../../store/images";
 
+//*                         CSS
 import "./ImageDetails.css";
 
 const ImageDetails = () => {
@@ -13,9 +16,6 @@ const ImageDetails = () => {
     const history = useHistory();
     let images = useSelector(state => state.images);
     images = Object.values(images);
-    // console.log(id)
-
-
 
     useEffect(() => {
         dispatch(getImages());
@@ -28,14 +28,13 @@ const ImageDetails = () => {
             .catch(err => console.log(err));
     };
 
-
     // const sessionUser = useSelector(state => state.session.user);
     const image = images.find(image => image.id === +id);
 
     return (
         <div className="image-details-page">
             <div className="image-details-page-image">
-                <img src={image?.imageUrl} alt={image.content} />
+                <img src={image?.imageUrl} />
             </div>
             <div className="image-details-page-info">
                 <div className="image-details-page-info-user">
