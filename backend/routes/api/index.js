46cb6837c37c119ff02cd29657e96cb9d5ee039f
Leo Lad <87998262+Dedition@users.noreply.github.com>
2022-05-21 19:01:15 -0400

    Comments router  added to index.js

--- backend/routes/api/index.js
+++ backend/routes/api/index.js
@@ -2,20 +2,14 @@ const router = require('express').Router();
 const sessionRouter = require('./session.js');
 const usersRouter = require('./users.js');
 const imagesRouter = require('./images.js');
+const commentsRouter = require('./comments.js');
 
 // Use the routers here
 // They will extend throughout the rest of your application
 router.use('/session', sessionRouter);
 router.use('/users', usersRouter);
 router.use('/images', imagesRouter);
-
-
-
-
-
-
-
-
+router.use('/comments', commentsRouter);
 
 
 
