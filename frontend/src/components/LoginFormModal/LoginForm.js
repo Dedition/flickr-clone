cee708939afb86751b896a17f18724138ecd1d66
Leo Lad <87998262+Dedition@users.noreply.github.com>
2022-05-27 17:00:24 -0400

    Createed bg animation for homepage and files for StartForFree component and Login component

--- frontend/src/components/LoginFormModal/LoginForm.js
+++ frontend/src/components/LoginFormModal/LoginForm.js
@@ -4,6 +4,8 @@ import * as sessionActions from '../../store/session';
 import { useDispatch, useSelector } from 'react-redux';
 import { Redirect } from 'react-router-dom';
 
+import { GiMountainCave } from 'react-icons/gi';
+
 import DemoUser from '../DemoUserButton/DemoUser';
 import './LoginForm.css';
 
@@ -29,31 +31,38 @@ function LoginForm() {
   }
 
   return (
-    <form onSubmit={handleSubmit}>
-      <ul>
-        {errors.map((error, idx) => <li key={idx}>{error}</li>)}
+    <form className="login-form" onSubmit={handleSubmit}>
+      <div className='login-logo'>
+        <GiMountainCave size={50} />
+      </div>
+      <div className="login-msg">
+        <h1>Login Here</h1>
+      </div>
+      <ul className="login-errors">
+        {errors.map((error, i) => (
+          <li key={i}>{error}</li>
+        ))}
       </ul>
-      <label>
-        Username or Email
-        <input
-          type="text"
-          value={credential}
-          onChange={(e) => setCredential(e.target.value)}
-          required
-        />
-      </label>
-      <label>
-        Password
-        <input
-          type="password"
-          value={password}
-          onChange={(e) => setPassword(e.target.value)}
-          required
-        />
-      </label>
-      <button type="submit">Log In</button>
+      <input
+        className="login-input"
+        type="text"
+        placeholder="Username or Email"
+        value={credential}
+        onChange={e => setCredential(e.target.value)}
+        required
+      />
+      <input
+        className="login-input"
+        type="password"
+        placeholder="Password"
+        value={password}
+        onChange={e => setPassword(e.target.value)}
+        required
+      />
+      <button className="login-button" type="submit">Log In</button>
       <DemoUser />
     </form>
+
   );
 }
 
---
+++ frontend/src/components/SplashPage/HomePage/StartForFreeModal.js
@@ -0,0 +1,3 @@
+import React, { useState } from 'react';
+import { useTranslation } from 'react-i18next';
+import { Modal } from "../../../context/Modal";
