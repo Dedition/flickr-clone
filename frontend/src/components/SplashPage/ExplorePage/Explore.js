adee88bebb813c0d011d61a5063e164026c467ba
Leo Lad <87998262+Dedition@users.noreply.github.com>
2022-05-28 21:19:08 -0400

    Added appropriate styling for explore page + container.

--- frontend/src/components/SplashPage/ExplorePage/Explore.js
+++ frontend/src/components/SplashPage/ExplorePage/Explore.js
@@ -3,6 +3,8 @@ import { useDispatch, useSelector } from 'react-redux';
 import { useEffect } from 'react';
 import { getImages } from '../../../store/images';
 
+import "./Explore.css";
+
 const Explore = () => {
     const dispatch = useDispatch();
     const history = useHistory();
@@ -21,9 +23,9 @@ const Explore = () => {
         <>
             <div className="explore-container">
                 <h1 style={{ paddingTop: '15px' }}>Welcome to the Gallery</h1>
-                <div>
+                <div className='image-container'>
                     {images.map(image => (
-                        <div className='images' key={image?.id}>
+                        <div className='explore-image' key={image?.id}>
                             {/* eslint-disable-next-line */}
                             <img onClick={() => { imageRedirect(image) }} key={image?.id} src={image?.imageUrl} />
                         </div>
