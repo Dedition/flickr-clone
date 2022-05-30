1b84876a15787b7f71aa9b4cddb468c47365056d
Leo Lad <87998262+Dedition@users.noreply.github.com>
2022-05-30 13:44:59 -0400

    Reformatted code on Comments.js. Styled it as well. Need to clean up imageDetails code

--- frontend/src/components/CommentsPage/Comments.js
+++ frontend/src/components/CommentsPage/Comments.js
@@ -8,6 +8,47 @@ import { createComment, getComment, removeComment } from '../../store/comments';
 
 //*                       Files
 import EditCommentFormModal from '../EditCommentForm/Edit';
+import "./Comments.css";
+
+// <div className="comments-container">
+//     <h2 id="comments-title">Comments</h2>
+//     {comments.map(comment => (
+//         <div className="comment-metadata" key={comment?.id}>
+//             <i className='fa-brands fa-pushed' id="fa-decor"></i>
+//             <p>{comment?.content}</p>
+//             <p>{comment?.createdAt}</p>
+//             <div className="body-comment">
+//                 <p>{comment?.comment}</p>
+//             </div>
+//             {sessionUser && sessionUser?.id === comment?.userId &&
+//                 <div className='delete-edit-icon'>
+//                     <button id={comment?.id} onClick={() => handleDelete(comment?.id)}>
+//                         <i className="fas fa-trash-alt"></i>
+//                     </button>
+//                     <EditCommentFormModal comment={comment} />
+//                 </div>
+//             }
+//         </div>
+//     ))}
+
+//     <div className="body-comment-form">
+//         <div className="comment-errors">
+//             {errors.map((error, i) => (
+//                 <p key={i}>{error}</p>
+//             ))}
+//         </div>
+//         <form onSubmit={handleSubmit}>
+//             <textarea
+//                 value={content}
+//                 onChange={e => setContent(e?.target?.value)}
+//                 placeholder="What do you think?"
+//             />
+//             <button type="submit" disabled={!!errors.length}>
+//                 <i className="fas fa-paper-plane"></i>
+//             </button>
+//         </form>
+//     </div>
+// </div>
 
 const Comments = () => {
     const { id } = useParams();
@@ -51,47 +92,39 @@ const Comments = () => {
     }, [content]);
 
     return (
-        <div className="comments-details">
-            <div className="comments-header">
-                <h2>Comments</h2>
-                <div className="comments-container">
-                    {comments.map(comment => (
-                        <div className="comment-metadata" key={comment?.id}>
-                            <p>{comment?.content}</p>
-                            <p>{comment?.createdAt}</p>
-                            <div className="body-comment">
-                                <p>{comment?.comment}</p>
+        <div className="comments-container">
+            <form className="comments-form" onSubmit={handleSubmit}>
+                <h2 id="comments-title">Comments</h2>
+                <textarea
+                    type="text"
+                    value={content}
+                    onChange={e => setContent(e.target.value)}
+                    placeholder="What do you think?"
+                    required
+                />
+                <button type="submit" id="add-comment-button">
+                    Add
+                </button>
+            </form>
+            {comments.map(comment => (
+                <div className="comment-item" key={comment.id}>
+                    <div>
+                        <i className='fa-brands fa-pushed' id="comment-icon" />
+                        <p>{comment?.content}</p>
+                        <span>{comment?.createdAt}</span>
+                        {sessionUser && sessionUser?.id === comment?.userId &&
+                            <div className='delete-edit-comp'>
+                                <button className="delete-comment-button" onClick={() => handleDelete(comment.id)}>
+                                    <i className="fa-solid fa-trash-alt" />
+                                </button>
+                                <EditCommentFormModal comment={comment} />
                             </div>
-                            {sessionUser && sessionUser?.id === comment?.userId &&
-                                <>
-                                    <button id={comment?.id} onClick={() => handleDelete(comment?.id)}>
-                                        <i className="fas fa-trash-alt"></i>
-                                    </button>
-                                    <EditCommentFormModal comment={comment} />
-                                </>
-                            }
-                        </div>
-                    ))}
+                        }
+                    </div>
                 </div>
-            </div>
-            <div className="body-comment-form">
-                <div className="login-errors">
-                    {errors.map((error, i) => (
-                        <p key={i}>{error}</p>
-                    ))}
-                </div>
-                <form onSubmit={handleSubmit}>
-                    <textarea
-                        value={content}
-                        onChange={e => setContent(e?.target?.value)}
-                        placeholder="What do you think?"
-                    />
-                    <button type="submit" disabled={!!errors.length}>
-                        <i className="fas fa-paper-plane"></i>
-                    </button>
-                </form>
-            </div>
+            ))}
         </div>
+
     );
 }
 
