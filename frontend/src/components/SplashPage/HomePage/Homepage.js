734ee80281bff9f25026183b6d3a3c3362566f25
Leo Lad <87998262+Dedition@users.noreply.github.com>
2022-05-30 20:17:02 -0400

    Completed homepage styling

--- frontend/src/components/SplashPage/HomePage/Homepage.js
+++ frontend/src/components/SplashPage/HomePage/Homepage.js
@@ -5,10 +5,10 @@ const Homepage = () => {
     return (
         <div className="homepage-container">
             <div className="center-text">
-                <h1>Find your inspiration.</h1>
-                <h2>Join the Flickr community, home to tens of billions of photos and 2 million groups.</h2>
+                <h1 id="splash-title">Find your inspiration.</h1>
+                <h2 id="splash-subheading">Join the Flickr community, home to tens of billions of photos and 2 million groups.</h2>
+                <StartForFreeModal />
             </div>
-            <StartForFreeModal />
         </div>
     );
 };
