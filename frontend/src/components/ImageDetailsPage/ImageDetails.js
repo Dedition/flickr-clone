cb1373339c313d26937db3f2e8090cc405c3c177
Leo Lad <87998262+Dedition@users.noreply.github.com>
2022-05-31 19:27:09 -0400

    Cleaned up code

--- frontend/src/components/ImageDetailsPage/ImageDetails.js
+++ frontend/src/components/ImageDetailsPage/ImageDetails.js
@@ -4,7 +4,6 @@ import { useDispatch, useSelector } from "react-redux";
 import { useParams, Link } from "react-router-dom";
 
 //*                         Store
-// import { deleteImageAction } from "../../store/images";
 import { getImages } from "../../store/images";
 
 //*                         Files & Components
@@ -13,58 +12,9 @@ import DeleteConfirm from "../DeleteButton/DeleteConfirm/DeleteConfirm";
 import Comments from "../CommentsPage/Comments";
 import "./ImageDetails.css";
 
-// <div className="image-details-container">
-//     <div className="image-details-header">
-//         <Link to="/" className="back-arrow"><i className="fas fa-arrow-left" style={{ fontSize: "13px" }}>{"  "} Back to Explore</i></Link>
-//     </div>
-//     {sessionUser && deleteEditLink}
-//     <div className="image-details-body">
-//         <div className="image-details-page-image">
-// <Link to={index > 0 ? `/images/${images[index - 1].id}` : null}
-//     className={index > 0 ? "forward-back-arrows" : "hidden"}>
-//     <i className="fas fa-chevron-left"></i>
-// </Link>
-// <Link to={index < images.length - 1 ? `/images/${images[index + 1].id}` : null}
-//     className={index < images.length - 1 ? "forward-back-arrows" : "hidden"}>
-//     <i className="fas fa-chevron-right"></i>
-// </Link>
-//             <h2>{image?.name}</h2>
-//             {/* eslint-disable-next-line*/}
-//             <img src={image?.imageUrl} />
-//         </div>
-//         <div className="image-details-page-info">
-//             <div className="image-details-page-info-user">
-//                 <div className="image-details-page-info-user-name">
-//                     <i className="fas fa-user-circle"></i>
-//                     <h3>Author: {image?.name}</h3>
-//                 </div>
-//             </div>
-//             <div className="image-details-page-info-location">
-//                 <div className="image-details-page-info-location-icon">
-// <i className="fas fa-camera-retro"></i>
-// <p>Captured at: {image?.location}</p>
-//                 </div>
-//                 <div className="image-details-page-info-location-date">
-// <i className="far fa-calendar-alt"></i>
-// <p>Uploaded on: {date}</p>
-//                 </div>
-//             </div>
-//             <div className="image-details-page-info-content">
-//                 <div className="image-details-page-info-content-icon">
-// <i className="fas fa-info-circle"></i>
-// <p>Description: {image?.content}</p>
-//                 </div>
-//             </div>
-//         </div>
-//     </div>
-//     <div className="image-details-comments">
-//         <Comments />
-//     </div>
-// </div>
 const ImageDetails = () => {
     const { id } = useParams();
     const dispatch = useDispatch();
-    // const history = useHistory();
 
     let images = useSelector(state => state.images);
     images = Object.values(images);
@@ -75,15 +25,7 @@ const ImageDetails = () => {
         dispatch(getImages());
     }, [dispatch]);
 
-    // const handleDelete = async (e) => {
-    //     e.preventDefault();
-    //     await dispatch(deleteImageAction(id))
-    //         .then(() => { history.push("/") })
-    //         .catch(err => console.log(err));
-    // };
-
     const sessionUser = useSelector(state => state.session.user);
-
 
     let date = new Date(image?.createdAt);
     date = date.toLocaleDateString();
