91f7866bb25fda0df6a1ba7921d5acbebc1962d5
Leo Lad <87998262+Dedition@users.noreply.github.com>
2022-05-31 23:49:19 -0400

    SO CLOSE TO GETTING THE JSON FILE TO SPREAD. It's not awaiting  correctly

--- backend/db/seeders/20220517231704-images.js
+++ backend/db/seeders/20220517231704-images.js
@@ -1,35 +1,67 @@
 'use strict';
+const { randomUrl } = require('../unsplashAPI/unsplash');
+const fetch = require('node-fetch');
+const newArr = [];
+
+// async function getRandomPhoto() {
+//   const locationsArr = ['London', 'Mexico', 'California', 'New Jersey', 'New Mexico', 'Chicago', 'El Salvador', 'Florida', 'Kentucky', 'Spain', 'Costa Rica', 'San Francisco,', 'New York', 'Barcelona', 'Madrid', 'Berlin', 'Rome', 'Paris', 'Kentucky', 'Amsterdam', 'Sweden']
+//   const res = await fetch(randomUrl);
+//   const json = await res.json();
+
+//   json.forEach((ele) => {
+//     newArr.push({
+//       userId: Math.floor(Math.random() * 3) + 1,
+//       name: 'Jesus',
+//       albumId: Math.floor(Math.random() * 3) + 1,
+//       imageUrl: ele.urls.regular,
+//       location: locationsArr[Math.floor(Math.random() * locationsArr.length)],
+//       content: 'lorem ipsum',
+//       createdAt: ele.created_at,
+//       updatedAt: ele.updated_at
+//     });
+//     console.log(newArr);
+//     return newArr;
+//   })
+// };
+
+// const locationsArr = ['London', 'Mexico', 'California', 'New Jersey', 'New Mexico', 'Chicago', 'El Salvador', 'Florida', 'Kentucky', 'Spain', 'Costa Rica', 'San Francisco,', 'New York', 'Barcelona', 'Madrid', 'Berlin', 'Rome', 'Paris', 'Kentucky', 'Amsterdam', 'Sweden']
+
+// const test = fetch(randomUrl)
+//   .then(response => response.json())
+//   .then(json => json.forEach((ele) => {
+//     newArr.push({
+//       userId: Math.floor(Math.random() * 3) + 1,
+//       name: 'Jesus',
+//       albumId: Math.floor(Math.random() * 3) + 1,
+//       imageUrl: ele.urls.regular,
+//       location: locationsArr[Math.floor(Math.random() * locationsArr.length)],
+//       content: 'lorem ipsum',
+//       createdAt: ele.created_at,
+//       updatedAt: ele.updated_at
+//     });
+//   }))
+
+// console.log(getRandomPhoto()
+//   .then((res) => res));
+
+// const arr = [];
+
+// const response = getRandomPhoto()
+//   .then(res => res.json())
+// console.log(response);
 
 module.exports = {
-  up: (queryInterface, Sequelize) => {
+  up: async (queryInterface, Sequelize) => {
     /*
       Add altering commands here.
       Return a promise to correctly handle asynchronicity.
 
       Example:
       */
-    return queryInterface.bulkInsert('Images', [{
-      userId: 1,
-      name: 'image1',
-      albumId: 1,
-      imageUrl: 'https://images.unsplash.com/photo-1518791841217-8f162f1e1131?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=800&q=60',
-      location: 'location1',
-      content: 'image1',
-    }, {
-      userId: 1,
-      name: 'image2',
-      albumId: 1,
-      imageUrl: 'https://images.unsplash.com/photo-1518791841217-8f162f1e1131?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=800&q=60',
-      location: 'location2',
-      content: 'image2',
-    }, {
-      userId: 1,
-      name: 'image3',
-      albumId: 1,
-      imageUrl: 'https://images.unsplash.com/photo-1518791841217-8f162f1e1131?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=800&q=60',
-      location: 'location3',
-      content: 'image3',
-    }], {});
+    const test = await getRandomPhoto();
+    return queryInterface.bulkInsert('Images',
+      test
+      , {});
   },
 
   down: (queryInterface, Sequelize) => {
---
+++ backend/json.js
@@ -0,0 +1,114 @@
+const object = {
+    id: "Y2ravKRtQZ0",
+    created_at: "2022-03-31T10:33:43-04:00",
+    updated_at: "2022-05-31T19:25:21-04:00",
+    promoted_at: null,
+    width: 8256,
+    height: 5504,
+    color: "#f3f3f3",
+    blur_hash: "LeLXSk4mD%IV~qNGIURiIUV@j]WC",
+    description: null,
+    alt_description: null,
+    urls: {
+        raw: "https://images.unsplash.com/photo-1648737153811-69a6d8c528bf?ixid=MnwzMzI0ODV8MXwxfGFsbHwxfHx8fHx8Mnx8MTY1NDA1MTcwMA&ixlib=rb-1.2.1",
+        full: "https://images.unsplash.com/photo-1648737153811-69a6d8c528bf?crop=entropy&cs=tinysrgb&fm=jpg&ixid=MnwzMzI0ODV8MXwxfGFsbHwxfHx8fHx8Mnx8MTY1NDA1MTcwMA&ixlib=rb-1.2.1&q=80",
+        regular: "https://images.unsplash.com/photo-1648737153811-69a6d8c528bf?crop=entropy&cs=tinysrgb&fit=max&fm=jpg&ixid=MnwzMzI0ODV8MXwxfGFsbHwxfHx8fHx8Mnx8MTY1NDA1MTcwMA&ixlib=rb-1.2.1&q=80&w=1080",
+        small: "https://images.unsplash.com/photo-1648737153811-69a6d8c528bf?crop=entropy&cs=tinysrgb&fit=max&fm=jpg&ixid=MnwzMzI0ODV8MXwxfGFsbHwxfHx8fHx8Mnx8MTY1NDA1MTcwMA&ixlib=rb-1.2.1&q=80&w=400",
+        thumb: "https://images.unsplash.com/photo-1648737153811-69a6d8c528bf?crop=entropy&cs=tinysrgb&fit=max&fm=jpg&ixid=MnwzMzI0ODV8MXwxfGFsbHwxfHx8fHx8Mnx8MTY1NDA1MTcwMA&ixlib=rb-1.2.1&q=80&w=200",
+        small_s3: "https://s3.us-west-2.amazonaws.com/images.unsplash.com/photo-1648737153811-69a6d8c528bf"
+    },
+    links: {
+        self: "https://api.unsplash.com/photos/Y2ravKRtQZ0",
+        html: "https://unsplash.com/photos/Y2ravKRtQZ0",
+        download: "https://unsplash.com/photos/Y2ravKRtQZ0/download?ixid=MnwzMzI0ODV8MXwxfGFsbHwxfHx8fHx8Mnx8MTY1NDA1MTcwMA",
+        download_location: "https://api.unsplash.com/photos/Y2ravKRtQZ0/download?ixid=MnwzMzI0ODV8MXwxfGFsbHwxfHx8fHx8Mnx8MTY1NDA1MTcwMA"
+    },
+    categories: [],
+    likes: 137,
+    liked_by_user: false,
+    current_user_collections: [],
+    sponsorship: {
+        impression_urls: [
+            "https://secure.insightexpressai.com/adServer/adServerESI.aspx?script=false&bannerID=10307206&rnd=[timestamp]&redir=https://secure.insightexpressai.com/adserver/1pixel.gif"
+        ],
+        tagline: "Closer to what you love",
+        tagline_url: "https://www.microsoft.com/windows/?ocid=UnsplashFY22_soc_pmc_win_",
+        sponsor: {
+            id: "kSlnstJTnY8",
+            updated_at: "2022-05-31T20:54:51-04:00",
+            username: "windows",
+            name: "Windows",
+            first_name: "Windows",
+            last_name: null,
+            twitter_username: "windows",
+            portfolio_url: "https://www.windows.com",
+            bio: "Follow us @Windows. #DoGreatThings",
+            location: null,
+            links: {
+                self: "https://api.unsplash.com/users/windows",
+                html: "https://unsplash.com/es/@windows",
+                photos: "https://api.unsplash.com/users/windows/photos",
+                likes: "https://api.unsplash.com/users/windows/likes",
+                portfolio: "https://api.unsplash.com/users/windows/portfolio",
+                following: "https://api.unsplash.com/users/windows/following",
+                followers: "https://api.unsplash.com/users/windows/followers"
+            },
+            profile_image: {
+                small: "https://images.unsplash.com/profile-1633364056312-0319b9fc4586image?ixlib=rb-1.2.1&crop=faces&fit=crop&w=32&h=32",
+                medium: "https://images.unsplash.com/profile-1633364056312-0319b9fc4586image?ixlib=rb-1.2.1&crop=faces&fit=crop&w=64&h=64",
+                large: "https://images.unsplash.com/profile-1633364056312-0319b9fc4586image?ixlib=rb-1.2.1&crop=faces&fit=crop&w=128&h=128"
+            },
+            instagram_username: "windows",
+            total_collections: 1,
+            total_likes: 0,
+            total_photos: 168,
+            accepted_tos: true,
+            for_hire: false,
+            social: {
+                instagram_username: "windows",
+                portfolio_url: "https://www.windows.com",
+                twitter_username: "windows",
+                paypal_email: null
+            }
+        }
+    },
+    topic_submissions: {},
+    user: {
+        id: "kSlnstJTnY8",
+        updated_at: "2022-05-31T20:54:51-04:00",
+        username: "windows",
+        name: "Windows",
+        first_name: "Windows",
+        last_name: null,
+        twitter_username: "windows",
+        portfolio_url: "https://www.windows.com",
+        bio: "Follow us @Windows. #DoGreatThings",
+        location: null,
+        links: {
+            self: "https://api.unsplash.com/users/windows",
+            html: "https://unsplash.com/es/@windows",
+            photos: "https://api.unsplash.com/users/windows/photos",
+            likes: "https://api.unsplash.com/users/windows/likes",
+            portfolio: "https://api.unsplash.com/users/windows/portfolio",
+            following: "https://api.unsplash.com/users/windows/following",
+            followers: "https://api.unsplash.com/users/windows/followers"
+        },
+        profile_image: {
+            small: "https://images.unsplash.com/profile-1633364056312-0319b9fc4586image?ixlib=rb-1.2.1&crop=faces&fit=crop&w=32&h=32",
+            medium: "https://images.unsplash.com/profile-1633364056312-0319b9fc4586image?ixlib=rb-1.2.1&crop=faces&fit=crop&w=64&h=64",
+            large: "https://images.unsplash.com/profile-1633364056312-0319b9fc4586image?ixlib=rb-1.2.1&crop=faces&fit=crop&w=128&h=128"
+        },
+        instagram_username: "windows",
+        total_collections: 1,
+        total_likes: 0,
+        total_photos: 168,
+        accepted_tos: true,
+        for_hire: false,
+        social: {
+            instagram_username: "windows",
+            portfolio_url: "https://www.windows.com",
+            twitter_username: "windows",
+            paypal_email: null
+        }
+    }
+}
