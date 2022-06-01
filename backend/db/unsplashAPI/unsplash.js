fce5f521e748f8c8029a0329a8ba22f7c455025b
Leo Lad <87998262+Dedition@users.noreply.github.com>
2022-06-01 16:38:10 -0400

    Cleaned up code

--- backend/db/unsplashAPI/unsplash.js
+++ backend/db/unsplashAPI/unsplash.js
@@ -5,53 +5,6 @@ const baseUrl = 'https://api.unsplash.com/';
 const accessToken = '&client_id=' + apiKey;
 const secretToken = '&client_secret=' + secret;
 const randomUrl = baseUrl + 'photos/?per_page=200' + accessToken;
-https://api.unsplash.com/search/photos?page=10&query=concert&client_id=I8IfHiK0vVi3EeEQyra7YkAZ4ICKd8yAHl-W7n5va1s
 
 
-module.exports = {
-    randomUrl,
-};
-
-
-
-
-// function searchPhotos(query) {
-//     return fetch(searchUrl + '&query=' + query)
-//         .then(response => console.log(response))
-//         .then(json => json.results);
-// }
-
-// return {
-//     getRandomPhoto,
-//     searchPhotos
-// }
-
-// const axios = require('axios');
-
-// const yourApiKey = '4c466510d35994482c1beec767684df9';
-
-// const data = {
-//     method: 'flickr.photos.search',
-//     api_key: yourApiKey,
-//     text: 'cat', // Search Text
-//     sort: 'interestingness-desc',
-//     per_page: 12,
-//     license: '4',
-//     extras: 'owner_name,license',
-//     format: 'json',
-//     nojsoncallback: 1,
-// };
-
-// const parameters = new URLSearchParams(data);
-// console.log(parameters);
-// const url = `https://api.flickr.com/services/rest/?${parameters}`;
-// console.log(url);
-
-// axios.get(url)
-//     .then(response => response.json())
-//     .then(data => {
-//         // We will impliment something later...
-//     });
-
-//     // To define axios, we need to install axios package.
-//     // To import axios, you write
+module.exports = { randomUrl };
